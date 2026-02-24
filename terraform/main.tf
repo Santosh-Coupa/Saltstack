293cb317b9c6384227c6e6c07785aea7655bae1e
Santosh Jadhav <santosh.jadhav@coupa.com>
2026-02-24 17:51:51 +0530

    Remove SSH key and ignore sensitive files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name = "rg-salt-lab"
 }
