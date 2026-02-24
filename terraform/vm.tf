1a9cc39110615f2d138dcd4c705bc2338ebf3366
Santosh Jadhav <santosh.jadhav@coupa.com>
2026-02-24 13:30:54 +0530

    Remove SSH key and ignore sensitive files

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -3,6 +3,7 @@ resource "azurerm_public_ip" "master_pip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
+  sku               = "Basic"
 }
 
 resource "azurerm_network_interface" "master_nic" {
