8debb9f6e9c0e22eca2f48063ce7b2848ca6bebb
Santosh Jadhav <santosh.jadhav@coupa.com>
2026-02-24 18:31:00 +0530

    Push first code

--- terraform/network.tf
+++ terraform/network.tf
@@ -24,10 +24,15 @@ resource "azurerm_network_security_rule" "salt_ports" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  source_port_range           = "*"
+  source_port_range           = "*" 
   destination_port_ranges     = ["22", "4505", "4506"]
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
