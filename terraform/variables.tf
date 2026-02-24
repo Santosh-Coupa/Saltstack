d19b4f38a0fdd56bcddf9cfff3ce4517f33a6b6e
Santosh Jadhav <santosh.jadhav@coupa.com>
2026-02-24 17:04:29 +0530

    Remove SSH key and ignore sensitive files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "location" { default = "centralindia" }
 variable "resource_group_name" { default = "rg-salt-lab" }
 variable "admin_username" { default = "azureuser" }
 variable "vm_size" { default = "Standard_B2ts_v2" }
-variable "minion_count" { default = 2 }
+variable "minion_count" { default = 1 }
 
 variable "ssh_public_key" {
   description = "SSH Public Key"
